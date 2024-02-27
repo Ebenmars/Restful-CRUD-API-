148e03a9ccae3a413b775acfe5f8db209f0b232e
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2024-02-27 14:27:55 -0500

    Added MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
 //express is node js frameowrk to help create node applications easily
-import express from "express";
+const express = require("express");
+
+//mongoose is the mongodb database
+const mongoose = require("mongoose");
+
+//for security so people dont see the url
+dotenv.config();
+
+//npm run dev when you are running nodemon
 
 const app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URL
+  .then(() => {
+    console.log("Connected to MongoDB");
+  }).catch((err) => {
+    console.log(err);
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+
+//root directory
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
+
